2b495c5cbe0989e2eebbaa84e5d088b7bb8d11af
Md. Yeasin Hossain <80535880+yeasin-md@users.noreply.github.com>
2022-11-08 22:45:35 +0600

    Create array.js

---
+++ array.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        _id:"01a",
+        name:"Yeasin",
+        roll:"011",
+        course:"CSE",
+        department:"ECE",
+        hobby:["Cricket","Code"]
+            
+    },
+     {
+        _id:"02a",
+        name:"Shahriar",
+        roll:"012",
+        course:"CSE",
+        department:"ECE",
+        hobby:["Football","Math","Code"]
+    },
+     {
+        _id:"03a",
+        name:"Sajjad",
+        roll:"013",
+        course:"EEE",
+        department:"ECE",
+        hobby:["Hockey","Cricket"]
+    }
+    ];
+    // console.log(data.filter(e=>e.course.includes("CSE")));
+    
+    // console.log(data.filter(e=>e.hobby.includes("Code" || "Cricket")).map(i=>i.name));
+    
+    // console.log(data.find(e=>e.name==="Yeasin"));
+    
+    // console.log(data.map(i=>i.hobby[0]));
